c1b8e9f6a57f40edc4f9d985899ff8483130e648
Aman Singh <haaamansingh007@gmail.com>
2023-02-09 19:48:06 +0530

    Updated

--- pages/image-tools/image-to-base.jsx
+++ pages/image-tools/image-to-base.jsx
@@ -13,19 +13,8 @@ export default function ImageToBase() {
           <main className="col-md-9 ms-sm-auto col-lg-9 col-xl-10 px-md-5 my-4">
             <Share
               name="Image to Base64 converter"
-              url="iamge-tools/image-to-base"
+              url="image-tools/image-to-base"
             />
-            <div className="row">
-              <div className="col-md-6 d-grid">
-                <Image
-                  src="https://cdn-icons-png.flaticon.com/512/8881/8881731.png"
-                  class="img-fluid border"
-                  alt="..."
-                  width={200}
-                  height={200}
-                ></Image>
-              </div>
-            </div>
           </main>
         </div>
       </div>
